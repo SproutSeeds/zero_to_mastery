88a2b604c0cb841b524c4774386995ffde3500d7
Cody Mitchell <codyshanemitchell@gmail.com>
2020-04-02 23:39:15 -0700

    completed through advanced loops

--- section_13/ES10.js
+++ section_13/ES10.js
@@ -4,26 +4,42 @@ console.log(numbers.flat(2));
 
 // .flat() will clean up the array and remove those empty indexes
 const names = [
-  ["bob", "cody"],
-  ["taylor", "carol"]
+  ['bob', 'cody'],
+  ['taylor', 'carol']
 ];
-console.log(".flat() on names array:", names.flat());
+console.log('.flat() on names array:', names.flat());
 
 // .flatMap() will do something for each item is flattened
 
-let new_names_array = names.flatMap(names => names + "bar");
+let new_names_array = names.flatMap(names => names + 'bar');
 console.log(new_names_array);
 
-let new_numbers_array = numbers.flatMap(number => number + "Wild");
+let new_numbers_array = numbers.flatMap(number => number + 'Wild');
 console.log(new_numbers_array);
 
-const useremail_1 = "          johnny@business.org".trimStart();
-const useremail_2 = "Donald@business.org           ".trimEnd();
-const useremail_3 = "     TRIMDonald@business.org           ".trim().length;
+const useremail_1 = '          johnny@business.org'.trimStart();
+const useremail_2 = 'Donald@business.org           '.trimEnd();
+const useremail_3 = '     TRIMDonald@business.org           '.trim().length;
 
 console.log(useremail_1);
 console.log(useremail_2);
 console.log(useremail_3);
 
+const userProfiles = [
+  ['commanderTom', 23],
+  ['derekZlander', 40],
+  ['hanzel', 18]
+];
 
-const userProfiles =[['commanderTom', 23]]
+// Object.fromEntries(array) will turn an array into an obj
+const objFromEntries = Object.fromEntries(userProfiles);
+console.log(objFromEntries);
+
+try {
+  console.log(Darling);
+} catch (error) {
+  console.log(
+    'We are now in the catch blog! Nice Catch! Your error is described as ',
+    error
+  );
+}
