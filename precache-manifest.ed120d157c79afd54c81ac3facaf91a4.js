fd37436179355d0e36d693dc9ad312929e199756
sproutseeds <codyshanemitchell@gmail.com>
2020-04-12 16:32:06 -0700

    Updates

---
+++ precache-manifest.ed120d157c79afd54c81ac3facaf91a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82defb83d2b8ae42fde4bf5da99dc7c9",
+    "url": "/SproutSeeds/zero_to_mastery_web_1/tree/master/section_19/robofriends/index.html"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/SproutSeeds/zero_to_mastery_web_1/tree/master/section_19/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "94bc436bc42185c2710c",
+    "url": "/SproutSeeds/zero_to_mastery_web_1/tree/master/section_19/robofriends/static/css/main.afcc7cb5.chunk.css"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/SproutSeeds/zero_to_mastery_web_1/tree/master/section_19/robofriends/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SproutSeeds/zero_to_mastery_web_1/tree/master/section_19/robofriends/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94bc436bc42185c2710c",
+    "url": "/SproutSeeds/zero_to_mastery_web_1/tree/master/section_19/robofriends/static/js/main.bda00cec.chunk.js"
+  },
+  {
+    "revision": "52fae4b9baab70e04249",
+    "url": "/SproutSeeds/zero_to_mastery_web_1/tree/master/section_19/robofriends/static/js/runtime-main.3a4df238.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/SproutSeeds/zero_to_mastery_web_1/tree/master/section_19/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
